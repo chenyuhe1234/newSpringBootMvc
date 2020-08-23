69a887836379371f2e56f457feb73d7ca4a13faf
chenyuhe1234 <37059012+chenyuhe1234@users.noreply.github.com>
2020-08-23 13:20:18 +0800

    通过@Value获取到application.properties中定义的hello的值
    通过@Value获取到application.properties中定义的hello的值

--- src/main/java/com/chenyuhe/cn/user/UserCtrl.java
+++ src/main/java/com/chenyuhe/cn/user/UserCtrl.java
@@ -1,10 +1,24 @@
 package com.chenyuhe.cn.user;
 
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/user")
 public class UserCtrl {
+
+
+	// 从application.properties中拿到想要的值  体现了约定优于配置
+	@Value("${Hello}")
+	private String name;
+
+	@RequestMapping("/test")
+	public String test() {
+
+		return name + "User Ctrl Hello World.....";
+	}
 
 
 }
